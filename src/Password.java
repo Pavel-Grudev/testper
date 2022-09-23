25e6d68549ef2527bf4606884f69edab36c25869
GPavel <grudevvpavel@yandex.ru>
2022-09-23 23:29:50 +0300

    All done

--- src/Password.java
+++ src/Password.java
@@ -9,7 +9,7 @@ public class Password {
         String PW = sc.nextLine();
         System.out.println("Ок. Идем дальше.");
 
-        
+
         if (PW.length()<8||PW.length()>15) {
             do {
                 System.out.println("Длина пароля должна составлять от 8 до 15 символов");
